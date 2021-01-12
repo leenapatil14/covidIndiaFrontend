ff6634b9708ed1fb96488c6e5e51ea16b3b97ad8
leenapatil14 <57280271+leenapatil14@users.noreply.github.com>
2021-01-12 00:57:40 -0500

    Updates

---
+++ precache-manifest.349c8bc25ae9cb23c4ddef362282303f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05671cdbc24f0d1d7ba0f9242494e85",
+    "url": "/covidIndiaFrontend/index.html"
+  },
+  {
+    "revision": "226160800145a7d7c1c0",
+    "url": "/covidIndiaFrontend/static/css/main.54c5e19c.chunk.css"
+  },
+  {
+    "revision": "d7c659dd7f2d782920dd",
+    "url": "/covidIndiaFrontend/static/js/2.4310bce3.chunk.js"
+  },
+  {
+    "revision": "e2154d38992f9d9bfd528d1b1522efad",
+    "url": "/covidIndiaFrontend/static/js/2.4310bce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226160800145a7d7c1c0",
+    "url": "/covidIndiaFrontend/static/js/main.2b776fbf.chunk.js"
+  },
+  {
+    "revision": "215a746266f05a4725ec",
+    "url": "/covidIndiaFrontend/static/js/runtime-main.8a91011d.js"
+  }
+]);
